f375c519a7d116b4934102070ae3d525e6c076cb
chessmito <chessmito@gmail.com>
2024-07-14 13:17:29 -0300

    ajustando altura dos elementos do header, precisa do logo ainda

--- app/page.js
+++ app/page.js
@@ -10,8 +10,8 @@ export default function Home() {
         height={100}
         alt ="logo"
         priority/> </li>
-        <li className="flex ">
-          <select className="bg-[#D4D4D4] rounded-md">
+        <li className="flex items-center ">
+          <select className="bg-[#D4D4D4] rounded-md h-10">
             <option className=""> Todos</option>
             <option> 1</option>
             <option> 2</option>
@@ -23,17 +23,17 @@ export default function Home() {
             <option> 8</option>
             <option> 9</option>
           </select>
-          <div className="flex relative">
-          <input id = "inputPesquisa"type='text'placeholder="Pesquisa devioteca" className=" text-center w-96 border-2 border-black rounded-md "></input>
+          <div className="flex items-center relative">
+          <input id = "inputPesquisa"type='text'placeholder="Pesquisa devioteca" className=" text-center  w-96 h-10 border-2 border-black rounded-md "></input>
           <Image src={'/lupa.png'}
-          className="bg-[#F3A847] p-2 rounded-md absolute left-80 "
-          width={67}
-          height={100}
+          className="bg-[#F3A847] p-2 rounded-md absolute left-80 ml-6 "
+          width={40}
+          height={30}
           priority/>
           </div>
         </li>
-        <li className="flex p-2">
-          <select className="rounded-md">
+        <li className="flex items-center p-2">
+          <select className="rounded-md h-10">
             <option>Login</option>
           </select>
           <Image src={"/img-cart.png"}
