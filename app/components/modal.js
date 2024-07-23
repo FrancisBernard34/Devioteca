b8d5c93427f434f0cdc252b1697d7088eb2c2775
chessmito <chessmito@gmail.com>
2024-07-23 12:16:30 -0300

    feito uma parte minuscula do modal

--- app/components/modal.js
+++ app/components/modal.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Modal() {
   return (
-    <div className='absolute bg-gray-700 flex flex-col z-50 w-80 h-80 rounded-br-lg rounded-bl-lg rounded-tr-lg'>Modal</div>
+    <div className='absolute bg-gray-700 flex flex-col  items-center z-50 w-80 h-80 rounded-br-lg rounded-bl-lg rounded-tr-lg'>
+      <h2 className='bg-yellow-400 text-center  mt-10 p-5 rounded-md  '>Faça seu login</h2>
+      <p className='mt-5'>Cliente novo? <a className="text-blue-300"href='#'>Clique aqui</a></p>
+      </div>
   )
 }
